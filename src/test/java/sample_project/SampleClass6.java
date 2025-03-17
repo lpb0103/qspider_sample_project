b9f1bc814d896d27e3c0c935f299a2bdcf0651c0
Laxmi Priya Behera <Laxmi Priya Behera@DESKTOP-DMRV8GN>
2025-03-18 02:12:41 +0530

    sixth commit , with println statement

--- src/test/java/sample_project/SampleClass6.java
+++ src/test/java/sample_project/SampleClass6.java
@@ -4,7 +4,7 @@ public class SampleClass6 {
 
 	public static void main(String[] args) {
 
-		System.out.println("I'm from SmapleClass5.java, after modify");
+		System.out.println("I'm from SmapleClass6.java, after modify");
 
 	}
 
