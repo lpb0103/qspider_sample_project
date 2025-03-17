d494c9689aa1f9c42e09ed95b89894f71bce1447
Laxmi Priya Behera <Laxmi Priya Behera@DESKTOP-DMRV8GN>
2025-03-18 01:42:43 +0530

    second commit

---
+++ src/test/java/sample_project/SampleClass2.java
@@ -0,0 +1,12 @@
+package sample_project;
+
+public class SampleClass2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I'm from SmapleClass2.java, after modify");
+
+		
+	}
+
+}
