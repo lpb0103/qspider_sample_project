448658448a7980b7dd7c313feb456bca26e2e9a2
Laxmi Priya Behera <Laxmi Priya Behera@DESKTOP-DMRV8GN>
2025-03-29 01:37:20 +0530

    commit for newbranch

---
+++ src/test/java/sample_project/SampleClass10.java
@@ -0,0 +1,16 @@
+package sample_project;
+
+public class SampleClass10 {
+
+	public static void main(String[] args) {
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+		System.out.println("I'm from SmapleClass10.java,yes after modify");
+
+	}
+
+}
